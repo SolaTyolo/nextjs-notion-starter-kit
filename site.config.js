92a3878f31fc35cc71a4e8e3b618055a668c172a
SolaTyolo <solat.yolo@gmail.com>
2022-04-05 02:32:04 +0800

    fix: 修改image-cdn-var-name

--- site.config.js
+++ site.config.js
@@ -33,7 +33,7 @@ module.exports = {
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
-  imageCDNHost: process.env.IMAGE_CDN,
+  imageCDNHost: process.env.NEXT_PUBLIC_IMAGE_CDN,
 
   // Utteranc.es comments via GitHub issue comments (optional)
   utterancesGitHubRepo: 'SolaTyolo/nextjs-notion-starter-kit',
