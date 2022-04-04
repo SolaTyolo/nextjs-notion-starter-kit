f3bd0315d047d8483aee7f3a177eee8b113085b9
SolaTyolo <solat.yolo@gmail.com>
2022-04-04 17:50:30 +0800

    fix: 修复collection===undefined的情况

--- lib/filter-published-posts.ts
+++ lib/filter-published-posts.ts
@@ -3,7 +3,8 @@ import { ExtendedRecordMap } from "notion-types";
 export async function filterPublishedPosts( recordMap: ExtendedRecordMap) {
     if( recordMap === undefined ) return;
     const blockIds = Object.keys(recordMap.block)
-    const collection = Object.values(recordMap.collection)[0].value
+    const collection = Object.values(recordMap.collection)[0]?.value
+    if (collection === undefined) return;
     const schemas = Object.keys(collection.schema)
     const publishedProperty = schemas.filter( s => {
       const sch = collection.schema[s]
